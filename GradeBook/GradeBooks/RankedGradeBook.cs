30c4abb05f27a4f7a9277c1f749c6e58003c3704
Kris de Maesschalck <kris.demaesschalck@katoennatie.com>
2018-02-27 14:35:44 +0100

    3.2 3.3 3.4 3.5

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -13,10 +14,33 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if(Students.Count < 5)
+            int totalStudents = Students.Count;
+            if(totalStudents < 5)
             {
                 throw new Exception("Ranked Grading requires a minimum of 5 students to work");
             }
+
+            int betterStudents = Students.Where(s => s.AverageGrade >= averageGrade).ToList().Count;
+
+            double percentage = ((double)betterStudents / (double)totalStudents);
+
+            if(percentage <= 0.20)
+            {
+                return 'A';
+            }
+            else if (percentage <= 0.40)
+            {
+                return 'B';
+            }
+            else if (percentage <= 0.60)
+            {
+                return 'C';
+            }
+            else if (percentage <= 0.80)
+            {
+                return 'D';
+            }
+
             return 'F';
         }
     }
